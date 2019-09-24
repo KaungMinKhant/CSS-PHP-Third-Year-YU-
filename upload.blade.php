c9f716aca3223a4b6d00741c902b931b2f3c25c0
kaungminkhant <phenomenalkaung@gmail.com>
2019-09-24 23:20:12 +0630

    khway table

---
+++ upload.blade.php
@@ -0,0 +1,6 @@
+<form action="upload.php" method="POST">
+
+	<input type="file" name="pp">
+	<input type="submit">
+	<img src="{{asset("$pp" )}}" alt="" width="200px" height="200px">
+</form>
---
+++ upload.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<?php 
+			$pp = $_POST["pp"];
+			echo $pp;
+		 ?>
+	</head>
+	<body>
+		<img src="$pp" alt="">
+	</body>
+</html>
